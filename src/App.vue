3c25f2767847032224c6f29994ba67c9f3d6eebf
javierSolanoP <jmsolano24@misena.edu.co>
2021-11-17 17:36:54 -0500

    Se aniade las primeras adecuaciones 'responsive'

--- src/App.vue
+++ src/App.vue
@@ -49,6 +49,8 @@ export default {
       if((user === 'javiers') && (password === '123')){
         this.denied = false;
         this.dashboard = true;
+        const access = document.querySelector('.access');
+        access.style.display = 'none';
         location.href = 'dashboard'
       }else{
         alert("Access denied!");
@@ -66,6 +68,7 @@ export default {
   font-weight: normal;  
   font-style: normal;  
 }
+
 *{
   margin: 0;
  
@@ -83,122 +86,248 @@ export default {
   visibility: visible;
 }
 
-.access{
-  width: 100%;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  visibility: visible;
+/* Para moviles */
+@media screen  and (max-width: 500px){
+  .access{
+    width: 100%;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #nav {
+    padding: 30px;
+  }
+
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
+
+  aside {
+    position: relative;
+    margin-left: 0%;
+    width: 100%;
+    height: 50vh;
+    background-color: #130999;
+  }
+
+  /* Formulario */
+  section{
+    position: relative;
+    margin-top: -40%;
+    width: 80%;
+    height: 60vh;
+    border-radius: 0.25rem;
+    border: none;
+    background-color: transparent;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .logo{
+    width: 100%;
+    height: 40%;
+    background-color: #fff;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    border: none;
+    border-top-right-radius: 50rem;
+    border-top-left-radius: 10rem;
+    border-right: none;
+    overflow: auto;
+  }
+  img{
+    width: 80%;
+    height: 90%;
+  }
+  form{
+    width: 100%;
+    height: 60%;
+    background-color: #fff;
+    border: none;
+    border-radius: 1rem;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+    border-bottom-left-radius: 0;
+    box-shadow: 0.5rem 0.5rem 1rem rgb(54, 54, 54);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  label{
+    width: 80%;
+    height: 10%;
+    font-family:Arial, Helvetica, sans-serif
+  }
+
+  input[type="text"], input[type="password"]{
+    width: 80%;
+    height: 10%;
+    border: 0.15rem  solid #ced4da;
+    border-radius: 10rem;
+  }
+
+  input[type="text"]:focus, input[type="password"]:focus {
+    color: #212529;
+    background-color: #fff;
+    border-color: #86b7fe;
+    outline: 0;
+    box-shadow: 0 0 0 0.25rem rgba(13, 110, 253, 0.25);
+  }
+
+  ::placeholder{
+    font-style: italic;
+  }
+
+  input[type="submit"]{
+    position:relative;
+    width: 80%;
+    height: 15%;
+    background-color: #130999;
+    border-radius: 10rem;
+    border: none;
+    cursor: pointer;
+    color: #fff;
+  }
+
+  input[type="submit"]:hover{
+    background-color: #F04509;
+  }
 }
 
-#nav {
-  padding: 30px;
-}
+/* Para ordenadores pequeños */
+@media screen and (min-width: 1232px){
+  .access{
+    width: 100%;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    visibility: visible;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  #nav {
+    padding: 30px;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
 
-aside {
-  position: relative;
-  margin-left: -20%;
-  width: 50%;
-  height: 100vh;
-  background-color: #130999;
-}
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
 
-/* Formulario */
-section{
-  position: relative;
-  margin-left: -30%;
-  width: 60%;
-  height: 50vh;
-  border-radius: 0.25rem;
-  border: none;
-  background-color: transparent;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-.logo{
-  width: 50%;
-  height: 100%;
-  background-color: #fff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #130999;
-  border-bottom-left-radius: 50rem;
-  border-top-left-radius: 20rem;
-  border-right: none;
-  overflow: auto;
-}
-img{
-  width: 90%;
-  height: 90%;
-}
-form{
-  width: 50%;
-  height: 100%;
-  background-color: #fff;
-  border: none;
-  border-radius: 1rem;
-  border-bottom-left-radius: 0;
-  box-shadow: 0.5rem 0.5rem 1rem rgb(54, 54, 54);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
+  aside {
+    position: relative;
+    margin-left: -20%;
+    width: 50%;
+    height: 100vh;
+    background-color: #130999;
+  }
 
-label{
-  width: 90%;
-  height: 10%;
-  font-family:Arial, Helvetica, sans-serif
-}
+  /* Formulario */
+  section{
+    position: relative;
+    margin-left: -30%;
+    width: 60%;
+    height: 50vh;
+    border-radius: 0.25rem;
+    border: none;
+    background-color: transparent;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .logo{
+    width: 50%;
+    height: 100%;
+    background-color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid #130999;
+    border-bottom-left-radius: 50rem;
+    border-top-left-radius: 20rem;
+    border-right: none;
+    overflow: auto;
+  }
+  img{
+    width: 90%;
+    height: 90%;
+  }
+  form{
+    width: 50%;
+    height: 100%;
+    background-color: #fff;
+    border: none;
+    border-radius: 1rem;
+    border-bottom-left-radius: 0;
+    box-shadow: 0.5rem 0.5rem 1rem rgb(54, 54, 54);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
-input[type="text"], input[type="password"]{
-  width: 80%;
-  height: 10%;
-  border: 0.15rem  solid #ced4da;
-  border-radius: 10rem;
-}
+  label{
+    width: 90%;
+    height: 10%;
+    font-family:Arial, Helvetica, sans-serif
+  }
 
-input[type="text"]:focus, input[type="password"]:focus {
-  color: #212529;
-  background-color: #fff;
-  border-color: #86b7fe;
-  outline: 0;
-  box-shadow: 0 0 0 0.25rem rgba(13, 110, 253, 0.25);
-}
+  input[type="text"], input[type="password"]{
+    width: 80%;
+    height: 10%;
+    border: 0.15rem  solid #ced4da;
+    border-radius: 10rem;
+  }
 
-::placeholder{
-  font-style: italic;
-}
+  input[type="text"]:focus, input[type="password"]:focus {
+    color: #212529;
+    background-color: #fff;
+    border-color: #86b7fe;
+    outline: 0;
+    box-shadow: 0 0 0 0.25rem rgba(13, 110, 253, 0.25);
+  }
 
-input[type="submit"]{
-  position:relative;
-  width: 80%;
-  height: 10%;
-  background-color: #130999;
-  border-radius: 10rem;
-  border: none;
-  cursor: pointer;
-  color: #fff;
-}
+  ::placeholder{
+    font-style: italic;
+  }
 
-input[type="submit"]:hover{
-  background-color: #F04509;
-}
+  input[type="submit"]{
+    position:relative;
+    width: 80%;
+    height: 10%;
+    background-color: #130999;
+    border-radius: 10rem;
+    border: none;
+    cursor: pointer;
+    color: #fff;
+  }
 
+  input[type="submit"]:hover{
+    background-color: #F04509;
+  }
+}
 </style>
--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -7,7 +7,10 @@
                     <br>
                     <p>Javier Solano</p>
                 </div>
-                <ul>
+                <button class="menu" @click="showMenu">
+                    <img src="menu.PNG" alt="Menú de navegación">
+                </button>
+                <ul id="ul">
                     <li>
                         <img src="hour.PNG" alt="" class="icon">
                         <router-link to="/">Horas</router-link>
@@ -56,6 +59,16 @@ export default {
                 // Redireccionamos a la vista: 
                 location.href = '/'
             }
+        },
+
+        // Metodo para mostrar el menu de navegacion: 
+        showMenu(){
+
+            // Seleccionamos la clase del elemento 'ul':
+            const ul = document.querySelector('#ul');
+
+            // Activamos la visibilidad del elemento: 
+            ul.style.display = 'flex';
         }
     
     }
@@ -68,88 +81,208 @@ export default {
     height: 100vh;
     display: flex;
 }
-aside{
-    top: 0;
-    position: sticky;
-    margin-left: 0;
-    width: 20%;
-    height: 100%;
-    background-color: #130999;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+
+/* Para moviles */
+@media screen  and (max-width: 500px){
+    .access{
+        display: none    
+    }
+    #dashboard{
+        flex-direction: column;
+    }
+    aside{
+        background-color: #130999;
+        width: 100%;
+        height: 25%;
+        margin-left: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .container-avatar{
+        width: 30%;
+        height: 100%;
+        margin-left: -3%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    .container-avatar .avatar{
+        width: 90%;
+        height: 60%;
+        border-radius: 100%;
+    }
+    .container-avatar p{
+        width: 100%;
+        color: #fff;
+    }
+    .menu{
+        width: 15%;
+        height: 40%;
+        margin-left: 8%;
+        border: none;
+        border-radius: 1rem;
+        background-color: #F04509;
+    }
+    .menu img{
+        width: 80%;
+        height: 80%;
+    }
+    ul{
+        float: left;;
+        margin-top: 40%;
+        width: 100vw;
+        height: 60vh;
+        display: none;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 0; 
+        background-color: #130999; 
+        transform: translateX(200px);
+        transform:rotateX(200px)
+    }
+    li{
+        border-bottom: 1px solid #fff;
+        width: 100%;
+        height: 13%;
+        color: #fff;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    li .icon{
+        margin-left:7%;
+        width: 15%;
+        height: 70%;
+        border-radius: 100%;
+    }
+    li a{
+        width: 75%;
+        height: 40%;
+        font-size: 150%;
+        color: #fff;
+        border: none;
+        cursor: pointer;
+        text-decoration: none;
+    }
+    li a:hover{
+        color: #F04509
+    }
+    .logout{
+        width: 15%;
+        height: 40%;
+        margin-left: 10%;
+        border: none;
+        border-radius: 1rem;
+        cursor: pointer;
+        background-color: #F04509;
+        color: #fff;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        
+    }
+    .logout .icon{
+        width: 80%;
+        height: 80%;
+    }
+    .logout p{
+        width: 100%;
+        display:none
+    }
 }
-.container-avatar{
-    width: 100%;
-    height: 30%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+/* Para ordenadores pequeños */
+@media screen and (min-width: 1232px){
+    aside{
+        top: 0;
+        position: sticky;
+        margin-left: 0;
+        width: 20%;
+        height: 100%;
+        background-color: #130999;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    .container-avatar{
+        width: 100%;
+        height: 30%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    .container-avatar .avatar{
+        width: 80%;
+        height: 80%;
+        border-radius: 100%;
+    }
+    .container-avatar p{
+        width: 100%;
+        color: #fff;
+    }
+    .menu{
+        display: none;
+    }
+    ul{
+        width: 90%;
+        height: 60%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 0;
+    }
+    li{
+        border-bottom: 1px solid #fff;
+        width: 100%;
+        height: 13%;
+        color: #fff;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    li .icon{
+        margin-left:7%;
+        width: 15%;
+        height: 70%;
+        border-radius: 100%;
+    }
+    li a{
+        width: 75%;
+        height: 40%;
+        font-size: 150%;
+        color: #fff;
+        border: none;
+        cursor: pointer;
+        text-decoration: none;
+    }
+    li a:hover{
+        color: #F04509
+    }
+    .logout{
+        width: 80%;
+        height: 5%;
+        border: none;
+        border-radius: 10rem;
+        cursor: pointer;
+        background-color: #F04509;
+        color: #fff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .logout .icon{
+        width: 20%;
+        height: 80%;
+    }
+    .logout p{
+        width: 70%;
+    }
 }
-.container-avatar .avatar{
-    width: 80%;
-    height: 80%;
-    border-radius: 100%;
-}
-.container-avatar p{
-    width: 100%;
-    color: #fff;
-}
-ul{
-    width: 90%;
-    height: 60%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 0;
-}
-li{
-    border-bottom: 1px solid #fff;
-    width: 100%;
-    height: 13%;
-    color: #fff;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-li .icon{
-    margin-left:7%;
-    width: 15%;
-    height: 70%;
-    border-radius: 100%;
-}
-li a{
-    width: 75%;
-    height: 40%;
-    font-size: 150%;
-    color: #fff;
-    border: none;
-    cursor: pointer;
-    text-decoration: none;
-}
-li a:hover{
-    color: #F04509
-}
-.logout{
-    width: 80%;
-    height: 5%;
-    border: none;
-    border-radius: 10rem;
-    cursor: pointer;
-    background-color: #F04509;
-    color: #fff;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-.logout .icon{
-    width: 20%;
-    height: 80%;
-}
-.logout p{
-    width: 70%;
-}
+
 </style>
